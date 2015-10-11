cdd1c6f76c707257fa5beb620b7f69835fd4acbb
Kanteev Leonid <lkolt2@mail.ru>
2015-10-11 23:29:59 +0300

    Update for fitsBits.c

--- fitsBits.c
+++ fitsBits.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int FitsBits(int x, int n)
+int fitsBits(int x, int n)
 {
-    int s = (x >> 31);                           // return -1: x < 0; 0: x >= 0;
-    int absx = ((x & ~s) | ((~x + 1) & s));      // return x: x >= 0; -x: x < 0;
-    int res = !(((absx + (~0 & s)) << 1) >> n);  // return ans;
+    int s = (x >> 31);                       	     // return -1: x < 0; 0: x >= 0;
+	int mone = ~0;                                   // -1
+    int absx = ((x & ~s) | ((~x + 1) & s));          // return x: x >= 0; -x: x < 0;
+    int res = !((absx + (mone & s)) >> (n + mone));  // return ans;
     return res;
 }
 
@@ -13,7 +14,6 @@ int main()
 {
     int x, n;
     scanf("%d %d", &x, &n);
-    printf("%d", FitsBits(x, n));
-
+    printf("%d", fitsBits(x, n));
     return 0;
 }
