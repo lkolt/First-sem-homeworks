84dfe671d8b5bb0bd05f5d69c0245f45fb04d3f2
Kanteev Leonid <lkolt2@mail.ru>
2015-10-22 18:04:10 +0300

    Update 3.cpp

--- 3.cpp
+++ 3.cpp
@@ -5,11 +5,13 @@ using namespace std;
 
 long double f_pow(long double a, int n)
 {
-    if (n == 0){
+    if (n == 0)
+    {
         return 1;
     }
 
-    if (n % 2 == 1){
+    if (n % 2 == 1)
+    }
         return f_pow(a, n - 1) * a;          // pow(i, n) = pow(i, n - 1) * i
     }
 
@@ -26,8 +28,10 @@ int main()
     cin >> a >> n;
     long double ans = f_pow(a, abs(n));
     if (n < 0)
+    {
         ans = 1 / ans;
-
+    }
+    
     cout << ans;
 
     return 0;
