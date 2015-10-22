762d45356264fad81f32a2d834d0e45ac8e728ad
Kanteev Leonid <lkolt2@mail.ru>
2015-10-22 18:04:51 +0300

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -8,11 +8,14 @@ int main()
     int n;
     cin >> n;    // number of element
     int ans = 0; // answer
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++)
+    {
         int cur; // cur element
         cin >> cur;
         if (cur == 0)
+        {
             ans++;
+        }
     }
 
     cout << ans; // print answer
